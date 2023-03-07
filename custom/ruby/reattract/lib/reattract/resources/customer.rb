fb57313ebae1f1e1c0cac3d6e214ed61af8e1c93
Justin Burris <713795+justinbburris@users.noreply.github.com>
2023-03-07 15:04:07 -0500

    allow a customer to be updated through api

--- custom/ruby/reattract/lib/reattract/resources/customer.rb
+++ custom/ruby/reattract/lib/reattract/resources/customer.rb
@@ -4,16 +4,23 @@ module Reattract
   # /v1/organization_customers
   class Customer
     class << self
-      def create(user_id:)
-        request.post(
+      def create(user_id:, **kwargs)
+        collection_request.post(
           body: {
-            organization_user_id: user_id
+            organization_user_id: user_id,
+            **kwargs
           }
         )
       end
 
+      def update(user_id:, **kwargs)
+        member_request(id: user_id).patch(
+          body: kwargs
+        )
+      end
+
       def list(limit: 20, page: 1, order: ['id desc'])
-        request.get(
+        collection_request.get(
           sort:       order,
           pagination: {
             limit: limit,
@@ -24,8 +31,12 @@ module Reattract
 
       private
 
-      def request
+      def collection_request
         Reattract::Request.new(path: '/organization_customers')
+      end
+
+      def member_request(id:)
+        Reattract::Request.new(path: "/organization_customers/#{id}")
       end
     end
   end
