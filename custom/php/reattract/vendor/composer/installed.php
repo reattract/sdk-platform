c73a88651e1881aa8f9fe4d8670a559d2f3b3245
Justin Burris <713795+justinbburris@users.noreply.github.com>
2023-04-18 15:56:41 -0400

    add php sdk

---
+++ custom/php/reattract/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'reattract/reattract',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7f70ff9e66669c4c7b5172c072e8b93ab220fc3b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'reattract/reattract' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7f70ff9e66669c4c7b5172c072e8b93ab220fc3b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
